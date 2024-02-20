5ff91525482b119c35b44fb5fdca499cd45efd0c
kpamukcu <kapawebtasarim@gmail.com>
2024-02-20 14:09:13 +0300

    api güncellendi

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-form.addEventListener('submit',function(form){
+form.addEventListener('submit', function (form) {
     form.preventDefault()
     let ipYakala = document.getElementById('ipAdres').value
-   
-    fetch(`https://api.ip2location.io/?key=37E09DA56F736FFE3240EC8620854B25&ip=${ipYakala}`)
+
+    fetch(`http://ip-api.com/json/${ipYakala}`)
         .then(response => response.json())
         .then(veri => {
             let ulke = document.getElementById('sonuc')
-            ulke.innerText = veri.ip
-            console.log(veri.ip)
-        })    
+            ulke.innerText = veri.country
+        })
 })
+
+
+
+
+
+//https://api.ip2location.io/?key=37E09DA56F736FFE3240EC8620854B25&ip=${ipYakala}
+
+
